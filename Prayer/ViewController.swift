68b529427bbbfdab0635c785331f23c31b3b9b6e
James <daoxingyue@live.cn>
2022-08-20 21:58:25 +0800

    Initial Commit

---
+++ Prayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prayer
+//
+//  Created by Daoxing.Huang on 2022/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
